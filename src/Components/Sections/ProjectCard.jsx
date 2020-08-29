c0b5fb5a128c74d720672706dee400f5c304086d
siewla <siewlapang@gmail.com>
2020-08-29 16:38:26 +0800

    amended css

--- src/Components/Sections/ProjectCard.jsx
+++ src/Components/Sections/ProjectCard.jsx
@@ -6,15 +6,18 @@ const ProjectCard = (props) => {
     const projectImgLink = props.projectImgLink
     const projectDemoLink = props.projectDemoLink
     const projectCodeLink = props.projectCodeLink
+
+
     return (
         <div className="project-card">
-            <h1 className="project-card-title">{projectTitle}</h1>
-            <img className="project-card-image" src={projectImgLink} alt="project" />
-            <h4>Description : <span className="project-description">{projectDescription}</span></h4>
-            <div className="project-card-footer">
+            <div className="project-card-actions">
                 <a className="footer-text" href={projectDemoLink} target="popout"><h4>Launch Demo <i className="fas fa-angle-double-right"></i></h4></a>
                 <a className="footer-text" href={projectCodeLink} target="popout"><h4>View Code <i className="fas fa-angle-double-right"></i></h4></a>
             </div>
+            <img className="project-card-image" src={projectImgLink} alt="project" />
+            <a href={projectDemoLink} className="project-card-title" target="popout">{projectTitle}</a>
+            <div className="horizontal-line"></div>
+            <h4>Description : <span className="project-description">{projectDescription}</span></h4>
         </div>
     )
 }
