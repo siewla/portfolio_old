1d54b5a3e5686bfb781cfac2af84065f6cae3e40
siewla <siewlapang@gmail.com>
2020-09-04 12:01:25 +0800

    make more mobile responsive for contact

--- src/Components/Sections/About.jsx
+++ src/Components/Sections/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     return (
         <div className="flex-column-center about">
             <h1>About</h1>
-            <p>Meet Siew La, Mechanical Engineer Turned Software Developer! Her six-year career as a mechanical engineer was moving slow, and she decided she needed a change.</p>
+            <h4 className="about-description">Meet Siew La, Mechanical Engineer Turned Software Developer! Her six-year career as a mechanical engineer was moving slow, and she decided she needed a change.</h4>
             <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" download>
             <MDBBtn className="btn btn-outline-black" type="submit">
                 Download My Resume <i className="fas fa-download"/>
