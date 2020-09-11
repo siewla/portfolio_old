006250c7aa2c5b6b74b8a2bef85c11750abaa342
siewla <siewlapang@gmail.com>
2020-09-11 16:43:24 +0800

    amended project details

--- src/Components/Sections/ProjectsDetails.js
+++ src/Components/Sections/ProjectsDetails.js
@@ -2,8 +2,8 @@ const  PROJECTS =[
     {
         id : '1',
         projectTitle : 'Brain Out - Cards',
-        projectStack: 'Built with HTML, CSS and JavaScript',
-        projectDescription : 'A gauntlet of card puzzles that involve interactions with cards. Players have to complete tasks like clicking on the highest valued card, or move a bunch of cards into a box, etc. There are currently 10 levels. The game inspiration is coming from a mobile game application named Brain Out. (available both in iOS and Android).',
+        projectStack: 'Built with HTML, CSS and JavaScript.',
+        projectDescription : 'A gauntlet of card puzzles that involve interactions with cards. Players have to complete tasks like clicking on the highest valued card, or move a bunch of cards into a box, etc. There are currently 10 levels.',
         projectImgLink : 'https://github.com/siewla/portfolio/blob/master/src/assets/project1.png?raw=true',
         projectDemoLink : 'https://siewla.github.io/brainout-cards/',
         projectCodeLink : 'https://github.com/siewla/brainout-cards',
@@ -23,7 +23,7 @@ const  PROJECTS =[
         id : '3',
         projectTitle : 'PinApps',
         projectStack: 'A full Stack application using MERN (MongoDB, React, Express and Node) Stack.',
-        projectDescription : 'MERN (MongoDB, React, Express and Node) Stack. This app serves to help its users organise and share the apps they commonly use.',
+        projectDescription : 'This application serves to help its users organise and share the apps they commonly use.',
         projectImgLink : 'https://github.com/siewla/portfolio/blob/master/src/assets/project3.png?raw=true',
         projectDemoLink : 'https://pinappsproject.herokuapp.com/',
         projectCodeLink : 'https://github.com/siewla/pinapps',
@@ -32,8 +32,8 @@ const  PROJECTS =[
     {
         id : '4',
         projectTitle : 'Sweet Accountant',
-        projectStack: 'A full Stack application using NERDS (Node, Express, React, Database using SQL) stack',
-        projectDescription : 'This app is a financial management app. This app also has charts and graphs which are extremely helpful in visualizing your expenditure against your income.',
+        projectStack: 'A full Stack application using NERDS (Node, Express, React, Database using SQL) stack.',
+        projectDescription : 'This application is a financial management app. This app also has charts and graphs which are extremely helpful in visualizing your expenditure against your income.',
         projectImgLink : 'https://github.com/siewla/portfolio/blob/master/src/assets/project4.png?raw=true',
         projectDemoLink : 'https://sweetaccountant.herokuapp.com/',
         collaborators : {name:'Linh Khanh ', link:'https://github.com/linhkhanh/'}
