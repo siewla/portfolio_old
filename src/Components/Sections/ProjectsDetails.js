e3e6e6975baf8032eb3fd9c9d9f36afd645ac39d
siewla <siewlapang@gmail.com>
2021-03-18 13:29:48 +0800

    change link

--- src/Components/Sections/ProjectsDetails.js
+++ src/Components/Sections/ProjectsDetails.js
@@ -1,43 +1,60 @@
-const  PROJECTS =[
-    {
-        id : '1',
-        projectTitle : 'Brain Out - Cards',
-        projectStack: 'Built with HTML, CSS and JavaScript.',
-        projectDescription : 'A gauntlet of card puzzles that involve interactions with cards. Players have to complete tasks like clicking on the highest valued card, or move a bunch of cards into a box, etc. There are currently 10 levels.',
-        projectImgLink : 'https://github.com/siewla/portfolio/blob/master/src/assets/project1.png?raw=true',
-        projectDemoLink : 'https://siewla.github.io/brainout-cards/',
-        projectCodeLink : 'https://github.com/siewla/brainout-cards',
-        collaborators: null
+const PROJECTS = [
+  {
+    id: '1',
+    projectTitle: 'Brain Out - Cards',
+    projectStack: 'Built with HTML, CSS and JavaScript.',
+    projectDescription:
+      'A gauntlet of card puzzles that involve interactions with cards. Players have to complete tasks like clicking on the highest valued card, or move a bunch of cards into a box, etc. There are currently 10 levels.',
+    projectImgLink:
+      'https://github.com/siewla/portfolio_old/blob/master/src/assets/project1.png?raw=true',
+    projectDemoLink: 'https://siewla.github.io/brainout-cards/',
+    projectCodeLink: 'https://github.com/siewla/brainout-cards',
+    collaborators: null,
+  },
+  {
+    id: '2',
+    projectTitle: 'Ideagram',
+    projectStack:
+      'A full Stack application with Express JS (Node JS) and Template Engine (EJS).',
+    projectDescription:
+      'This application acts as a visual discovery engine for finding ideas like countries, recipes, home and style inspiration, and many more.',
+    projectImgLink:
+      'https://github.com/siewla/portfolio_old/blob/master/src/assets/project2.png?raw=true',
+    projectDemoLink: 'http://ideagram.herokuapp.com/',
+    projectCodeLink: 'https://github.com/siewla/ideagram',
+    collaborators: null,
+  },
+  {
+    id: '3',
+    projectTitle: 'PinApps',
+    projectStack:
+      'A full Stack application using MERN (MongoDB, React, Express and Node) Stack.',
+    projectDescription:
+      'This application serves to help its users organise and share the apps they commonly use.',
+    projectImgLink:
+      'https://github.com/siewla/portfolio_old/blob/master/src/assets/project3.png?raw=true',
+    projectDemoLink: 'https://pinappsproject.herokuapp.com/',
+    projectCodeLink: 'https://github.com/siewla/pinapps',
+    collaborators: {
+      name: 'Chee Kiong Neo',
+      link: 'https://github.com/neocheekiong',
     },
-    {
-        id : '2',
-        projectTitle : 'Ideagram',
-        projectStack: 'A full Stack application with Express JS (Node JS) and Template Engine (EJS).',
-        projectDescription : 'This application acts as a visual discovery engine for finding ideas like countries, recipes, home and style inspiration, and many more.',
-        projectImgLink : 'https://github.com/siewla/portfolio/blob/master/src/assets/project2.png?raw=true',
-        projectDemoLink : 'http://ideagram.herokuapp.com/',
-        projectCodeLink : 'https://github.com/siewla/ideagram',
-        collaborators: null
+  },
+  {
+    id: '4',
+    projectTitle: 'Sweet Accountant',
+    projectStack:
+      'A full Stack application using NERDS (Node, Express, React, Database using SQL) stack.',
+    projectDescription:
+      'This application is a financial management app. This app also has charts and graphs which are extremely helpful in visualizing your expenditure against your income.',
+    projectImgLink:
+      'https://github.com/siewla/portfolio_old/blob/master/src/assets/project4.png?raw=true',
+    projectDemoLink: 'https://sweetaccountant.herokuapp.com/',
+    collaborators: {
+      name: 'Linh Khanh ',
+      link: 'https://github.com/linhkhanh/',
     },
-    {
-        id : '3',
-        projectTitle : 'PinApps',
-        projectStack: 'A full Stack application using MERN (MongoDB, React, Express and Node) Stack.',
-        projectDescription : 'This application serves to help its users organise and share the apps they commonly use.',
-        projectImgLink : 'https://github.com/siewla/portfolio/blob/master/src/assets/project3.png?raw=true',
-        projectDemoLink : 'https://pinappsproject.herokuapp.com/',
-        projectCodeLink : 'https://github.com/siewla/pinapps',
-        collaborators : {name:'Chee Kiong Neo', link:'https://github.com/neocheekiong'}
-    },
-    {
-        id : '4',
-        projectTitle : 'Sweet Accountant',
-        projectStack: 'A full Stack application using NERDS (Node, Express, React, Database using SQL) stack.',
-        projectDescription : 'This application is a financial management app. This app also has charts and graphs which are extremely helpful in visualizing your expenditure against your income.',
-        projectImgLink : 'https://github.com/siewla/portfolio/blob/master/src/assets/project4.png?raw=true',
-        projectDemoLink : 'https://sweetaccountant.herokuapp.com/',
-        collaborators : {name:'Linh Khanh ', link:'https://github.com/linhkhanh/'}
-    },
-]
+  },
+];
 
-export default PROJECTS
+export default PROJECTS;
